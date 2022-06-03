b7dcbd1d46efa7a7eeef9b177b539530b256330b
Francois Fernandes <francois.fernandes@digitalfrontiers.de>
2022-06-03 17:36:58 +0200

    Rough sketch of a frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Post} from "./components/post"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Post post={
+
+          {
+            id: "123",
+            user: {username: "alfons", displayName: "Alfons User"},
+            message: "this is my most important message of the day!",
+            postedAt: "1h"
+          }
+          
+        }></Post>
+      </div>
   );
 }
 
